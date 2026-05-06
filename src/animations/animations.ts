66f4005feabf52f1c3f7809e860c1587cb76f618
Kartik-Nama <kartik.nama@mechlintech.com>
2026-05-07 01:12:04 +0530

    Revert "Merge pull request #7 from akshat-mechlin/feature/added-designs-and-features"
    This reverts commit 650c75439b15f60e05165a54978feaf27450f013, reversing
    changes made to 778e750e3d86c2a961154efc62e945616b36500e.

---
+++ src/animations/animations.ts
@@ -0,0 +1,31 @@
+import { Variants } from "framer-motion";
+
+export const fadeInUp: Variants = {
+  hidden: {
+    opacity: 0,
+    y: 40,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      ease: "easeOut",
+    },
+  },
+};
+
+export const fadeInRight: Variants = {
+  hidden: {
+    opacity: 0,
+    x: 40,
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 1,
+      ease: "easeOut",
+    },
+  },
+};
